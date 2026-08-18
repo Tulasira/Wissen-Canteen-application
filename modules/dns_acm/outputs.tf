b990b4c44e0e732f7fa49df1ad19de28c3130c90
Tulasira <tulasiram185@gmail.com>
2026-08-18 15:38:55 +0530

    Remove existing Terraform code

--- modules/dns_acm/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "api_certificate_arn" {
-  value = aws_acm_certificate_validation.api.certificate_arn
-}
-
-output "web_certificate_arn" {
-  value = aws_acm_certificate_validation.web.certificate_arn
-}
--- modules/elasticache/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "redis_endpoint" {
-  value = aws_elasticache_cluster.this.cache_nodes[0].address
-}
-
-output "redis_secret_arn" {
-  value = aws_secretsmanager_secret.redis_url.arn
-}
